f7edee7b01773d067e9ae7ece0c79cd8c7782462
dmigra <dmitriigrakov@gmail.com>
2018-11-22 19:02:21 +1000

    Swipe carousel and remove script code from html

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,37 @@
 $(document).ready(function(){
   /* Intro
   **************************************************/
- 
+  if ($("body").data("title") === "introPage") {
+    //  Init Carousel
+    $("#intro .owl-carousel").owlCarousel({
+      loop: true,
+      margin: 10,
+      nav: false,
+      animateOut: 'fadeOut',
+      autoplay: true,
+      autoplayTimeout: 5000,
+      autoplayHoverPause: true,
+      responsive: {
+          0: { items: 1 }
+      }
+    });  
+  }
   /* Enlight
   **************************************************/
-  // Show referred popup window
-  $('.showPopup').on('click', function() {
-    $($(this).data('root')).css('display', 'none');
-    $($(this).data('target')).css('display', 'block');
-  });
-  // Hide referred popup window
-  $('.hidePopup').on('click', function() {
-    $($(this).data('root')).css('display', 'none');
-    $($(this).data('target')).css('display', 'block');
-  });
-  
+  if ($("body").data("title") == "enlightPage") {
+    $('#enlightCarousel').carousel().swipeCarousel({
+      // options here
+      sensitivity: 'high'
+    });
+     // Show referred popup window
+    $('.showPopup').on('click', function() {
+      $($(this).data('root')).css('display', 'none');
+      $($(this).data('target')).css('display', 'block');
+    });
+    // Hide referred popup window
+    $('.hidePopup').on('click', function() {
+      $($(this).data('root')).css('display', 'none');
+      $($(this).data('target')).css('display', 'block');
+    });
+  }
 });
