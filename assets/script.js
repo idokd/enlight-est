e5c3110409505f5a5bef2b1386baa28c8750b2b5
dmigra <dmitriigrakov@gmail.com>
2018-11-22 12:03:39 +1000

    Finished milestone1

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,18 @@
 $(document).ready(function(){
   /* Intro
   **************************************************/
-  // Init Carousel
-  // $("#intro .owl-carousel").owlCarousel({
-  //   loop: true,
-  //   margin: 10,
-  //   nav: false,
-  //   animateOut: 'fadeOut',
-  //   autoplay: true,
-  //   autoplayTimeout: 5000,
-  //   autoplayHoverPause: true,
-  //   responsive: {
-  //       0: { items: 1 }
-  //   }
-  // });
+ 
   /* Enlight
   **************************************************/
-  // Show garranty popup window
-  $('.garranty-item').on('click', function() {
-    $('#enlight').css('display', 'none');
-    $('#popups .garranty-popup').css('display', 'block');
+  // Show referred popup window
+  $('.showPopup').on('click', function() {
+    $($(this).data('root')).css('display', 'none');
+    $($(this).data('target')).css('display', 'block');
   });
+  // Hide referred popup window
+  $('.hidePopup').on('click', function() {
+    $($(this).data('root')).css('display', 'none');
+    $($(this).data('target')).css('display', 'block');
+  });
+  
 });
